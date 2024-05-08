a542a8d8031f707ef1070209e678dcbc6770a3ab
RagulRavichandran <ragulrpr1@gmail.com>
2024-05-08 16:46:50 +0530

    websocket works want's to add count

--- ExcelDemo/src/main/java/com/excel/controller/ExcelController.java
+++ ExcelDemo/src/main/java/com/excel/controller/ExcelController.java
@@ -4,33 +4,25 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.excel.service.ExcelService;
 
-@CrossOrigin
 @RestController
 public class ExcelController {
 
     @Autowired
     private ExcelService excelService;
 
-    @MessageMapping("/fetchExcel")
-    @SendTo("/topic/excelData")
-    public List<String[]> sendExcelData() {
-        return excelService.getData();
-    }
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
 
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(fixedRate = 2000) // every 2 seconds
     public void sendDataAutomatically() {
-//        sendExcelData();
-        List<String[]> data = sendExcelData();
-
-        System.out.println("Sending data: " + Arrays.deepToString(data.toArray()));
-
+        List<String[]> excelData = excelService.getData();
+        messagingTemplate.convertAndSend("/topic/excelData", excelData);
+        System.out.println("excelData" + Arrays.deepToString(excelData.toArray()));
     }
 }
